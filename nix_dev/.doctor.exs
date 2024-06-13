63b2b3655fc71c85e6b6cff8cdecbde5f4246022
Nıcklas Moberg <2671356+nicksen@users.noreply.github.com>
2024-06-13 05:58:56 +0200

    unify doctor config globally

--- nix_dev/.doctor.exs
+++ nix_dev/.doctor.exs
@@ -1,15 +1,2 @@
-%Doctor.Config{
-  ignore_modules: [],
-  ignore_paths: [],
-  min_module_doc_coverage: 100,
-  min_module_spec_coverage: 100,
-  min_overall_doc_coverage: 100,
-  min_overall_moduledoc_coverage: 100,
-  min_overall_spec_coverage: 100,
-  exception_moduledoc_required: true,
-  raise: false,
-  reporter: Doctor.Reporters.Short,
-  struct_type_spec_required: true,
-  umbrella: false,
-  failed: false
-}
+{opts, []} = Code.eval_file("../doctor.config.exs", __DIR__)
+opts
